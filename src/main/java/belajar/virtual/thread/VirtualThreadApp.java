e3518fbade66363c67d3113a55793050b62db9df
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-09-25 13:28:55 +0700

    virtual thread

--- src/main/java/belajar/virtual/thread/VirtualThreadApp.java
+++ src/main/java/belajar/virtual/thread/VirtualThreadApp.java
@@ -1,12 +1,13 @@
 package belajar.virtual.thread;
 
+import java.io.IOException;
 import java.time.Duration;
 
-public class PlatformThreadApp {
+public class VirtualThreadApp {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     for (int i = 0; i < 10_000; i++) {
-      Thread thread = Thread.ofPlatform().name("eko-" + i).daemon(false).unstarted(() -> {
+      Thread thread = Thread.ofVirtual().name("eko-" + i).unstarted(() -> {
         try {
           Thread.sleep(Duration.ofSeconds(2));
           System.out.println("Hello " + Thread.currentThread() + "!");
@@ -18,5 +19,6 @@ public class PlatformThreadApp {
       thread.start();
     }
 
+    System.in.read();
   }
 }
